82bf5c8fad231a89e733043d2112258bb19f512c
quzhengguo <quzhengguo@sinotrans.com>
2022-03-29 16:15:13 +0800

    demo、redis  优化

--- springboot-redis/src/main/java/com/example/springbootredis/SpringbootRedisApplication.java
+++ springboot-redis/src/main/java/com/example/springbootredis/SpringbootRedisApplication.java
@@ -2,8 +2,10 @@ package com.example.springbootredis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringbootRedisApplication {
 
     public static void main(String[] args) {
