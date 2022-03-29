7fce53b31bded95d5f91ebab7805cf8e1c459bc7
quzhengguo <quzhengguo@sinotrans.com>
2022-03-29 16:14:16 +0800

    spring-controller: 1)请求参数 2)打包元数据 3)swagger 4)消息转换器 5)入参返回xml

---
+++ spring-controller/src/main/java/com/example/springcontroller/SpringControllerApplication.java
@@ -0,0 +1,14 @@
+package com.example.springcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringControllerApplication.class, args);
+    }
+
+}
