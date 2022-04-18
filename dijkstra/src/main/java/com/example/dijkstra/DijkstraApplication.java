4e967b8d8fc85c4b0ee8fd16c308a07b5ff97568
quzhengguo <quzhengguo@sinotrans.com>
2022-04-18 11:19:15 +0800

    Dijkstra： 最短路径算法

--- dijkstra/src/main/java/com/example/dijkstra/DijkstraApplication.java
+++ dijkstra/src/main/java/com/example/dijkstra/DijkstraApplication.java
@@ -1,5 +1,6 @@
 package com.example.dijkstra;
 
+import com.example.dijkstra.bo.Graph;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DijkstraApplication {
 
     public static void main(String[] args) {
+
+        Graph<String> graph = new Graph<>(7);
+        graph.setDatas(new String[]{"A", "B", "C", "D", "E", "F", "G"});
+        int[][] matrix = {
+                {0, 7, 3, 2, 2, 0, 0},
+                {0, 0, 1, 0, 0, 0, 0},
+                {0, 0, 0, 0, 4, 3, 0},
+                {0, 0, 0, 0, 1, 10, 2},
+                {0, 0, 0, 0, 0, 4, 2},
+                {0, 0, 0, 0, 0, 0, 7},
+                {0, 0, 0, 0, 0, 0, 0}};
+        graph.setMatrix(matrix);
+        graph.makeUndirected();
+        for (int i = 0; i < 7; i++) {
+            graph.initStatuses();
+            graph.DijkstraPath(i);
+        }
+
         SpringApplication.run(DijkstraApplication.class, args);
     }
 
--- dijkstra/src/main/java/com/example/dijkstra/bo/Graph.java
+++ dijkstra/src/main/java/com/example/dijkstra/bo/Graph.java
@@ -199,22 +199,4 @@ public class Graph<T> {
         return minIndex;
     }
 
-    public static void main(String[] args) {
-        Graph<String> graph = new Graph<>(7);
-        graph.setDatas(new String[]{"A", "B", "C", "D", "E", "F", "G"});
-        int[][] matrix = {
-                {0, 7, 3, 2, 2, 0, 0},
-                {0, 0, 1, 0, 0, 0, 0},
-                {0, 0, 0, 0, 4, 3, 0},
-                {0, 0, 0, 0, 1, 10, 2},
-                {0, 0, 0, 0, 0, 4, 2},
-                {0, 0, 0, 0, 0, 0, 7},
-                {0, 0, 0, 0, 0, 0, 0}};
-        graph.setMatrix(matrix);
-        graph.makeUndirected();
-        for (int i = 0; i < 7; i++) {
-            graph.initStatuses();
-            graph.DijkstraPath(i);
-        }
-    }
 }
